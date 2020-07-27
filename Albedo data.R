d001eb6e30dfd22896f1dba8044e92f18988bc41
Tyler Pantle <TylerPantle@Tylers-MacBook-Pro-2.local>
2020-07-27 19:41:39 -0400

    subset data for snow on/off months

--- Albedo data.R
+++ Albedo data.R
@@ -21,8 +21,14 @@ library(dplyr)
 library(stringr)
 library(tidyverse)
 
+###################################
+#
 #extract month and day from each data set
-#tas avg
+#
+
+#tasman avg
+tas_avg$system.time_start <- as.Date(tas_avg$system.time_start,format = "%m/%d/%y")
+
 new_tas_avg = tas_avg %>% 
   dplyr::mutate(year = lubridate::year(system.time_start),
                 month = lubridate::month(system.time_start),
@@ -32,7 +38,7 @@ head(new_tas_avg) #shows first 6 rows
 
 new_tas_avg #shows all rows
 
-#tas
+#tasman
 #reformat date
 tas$system.time_start <- as.Date(tas$system.time_start,format = "%b %d,%Y")
 
@@ -43,10 +49,9 @@ new_tas = tas %>%
 
 head(new_tas) #shows first 6 rows
 
-#fj_avg
-#reformat date
+#franz joseph avg
 fj_avg$system.time_start <- as.Date(fj_avg$system.time_start,format = "%b %d,%Y")
-#extract dates
+
 new_fj_avg = fj_avg %>% 
   dplyr::mutate(year = lubridate::year(system.time_start),
                 month = lubridate::month(system.time_start),
@@ -54,10 +59,9 @@ new_fj_avg = fj_avg %>%
 
 head(new_fj_avg) #shows first 6 rows
 
-#fj
-#reformat date
+#franz joseph
 fj$system.time_start <- as.Date(fj$system.time_start,format = "%b %d,%Y")
-#extract dates
+
 new_fj = fj %>% 
   dplyr::mutate(year = lubridate::year(system.time_start),
                 month = lubridate::month(system.time_start),
@@ -65,10 +69,9 @@ new_fj = fj %>%
 
 head(new_fj) #shows first 6 rows
 
-#fox_avg
-#reformat date
+#fox avg
 fox_avg$system.time_start <- as.Date(fox_avg$system.time_start,format = "%b %d,%Y")
-#extract dates
+
 new_fox_avg = fox_avg %>% 
   dplyr::mutate(year = lubridate::year(system.time_start),
                 month = lubridate::month(system.time_start),
@@ -77,9 +80,8 @@ new_fox_avg = fox_avg %>%
 head(new_fox_avg) #shows first 6 rows
 
 #fox
-#reformat date
 fox$system.time_start <- as.Date(fox$system.time_start,format = "%b %d,%Y")
-#extract dates
+
 new_fox = fox %>% 
   dplyr::mutate(year = lubridate::year(system.time_start),
                 month = lubridate::month(system.time_start),
@@ -87,13 +89,22 @@ new_fox = fox %>%
 
 head(new_fox) #shows first 6 rows
 
-#Franz Josph Avg Albedo over season
+###################################
+#
+# add column with season
+#
+
+#Franz Josph Avg Albedo 
 new_fj_avg$season[new_fj_avg$month==12]<-"Summer"
 new_fj_avg$season[new_fj_avg$month>=1 & new_fj_avg$month<=2]<-"Summer"
 new_fj_avg$season[new_fj_avg$month>=3 & new_fj_avg$month<=5]<-"Autumn"
 new_fj_avg$season[new_fj_avg$month>=6 & new_fj_avg$month<=8]<-"Winter"
 new_fj_avg$season[new_fj_avg$month>=9 & new_fj_avg$month<=11]<-"Spring"
-#boxplot of albedo over season
+
+new_fj_avg$snow[new_fj_avg$month>=6 & new_fj_avg$month<=10]<-"On"
+new_fj_avg$snow[new_fj_avg$month>=1 & new_fj_avg$month<=5]<-"Off"
+new_fj_avg$snow[new_fj_avg$month>=11 & new_fj_avg$month<=12]<-"Off"
+#boxplot of albedo throughout seasons
 boxplot(
   new_fj_avg$fj~new_fj_avg$season, 
   new_fj_avg,
@@ -101,13 +112,17 @@ boxplot(
   xlab = "Season",
   ylab = "Average Albedo")
 
-#Fox  Avg Albedo over season
+#Fox Avg Albedo 
 new_fox_avg$season[new_fox_avg$month==12]<-"Summer"
 new_fox_avg$season[new_fox_avg$month>=1 & new_fox_avg$month<=2]<-"Summer"
 new_fox_avg$season[new_fox_avg$month>=3 & new_fox_avg$month<=5]<-"Autumn"
 new_fox_avg$season[new_fox_avg$month>=6 & new_fox_avg$month<=8]<-"Winter"
 new_fox_avg$season[new_fox_avg$month>=9 & new_fox_avg$month<=11]<-"Spring"
-#boxplot of albedo over season
+
+new_fox_avg$snow[new_fox_avg$month>=6 & new_fox_avg$month<=10]<-"On"
+new_fox_avg$snow[new_fox_avg$month>=1 & new_fox_avg$month<=5]<-"Off"
+new_fox_avg$snow[new_fox_avg$month>=11 & new_fox_avg$month<=12]<-"Off"
+#boxplot of albedo throughout seasons
 boxplot(
   new_fox_avg$fox~new_fox_avg$season, 
   new_fox_avg,
@@ -115,18 +130,34 @@ boxplot(
   xlab = "Season",
   ylab = "Average Albedo")
 
-#Tasman Avg Albedo over season
+#Tasman Avg Albedo
 new_tas_avg$season[new_tas_avg$month==12]<-"Summer"
 new_tas_avg$season[new_tas_avg$month>=1 & new_tas_avg$month<=2]<-"Summer"
 new_tas_avg$season[new_tas_avg$month>=3 & new_tas_avg$month<=5]<-"Autumn"
 new_tas_avg$season[new_tas_avg$month>=6 & new_tas_avg$month<=8]<-"Winter"
 new_tas_avg$season[new_tas_avg$month>=9 & new_tas_avg$month<=11]<-"Spring"
-#boxplot of albedo over season
+
+new_tas_avg$snow[new_tas_avg$month>=6 & new_tas_avg$month<=10]<-"On"
+new_tas_avg$snow[new_tas_avg$month>=1 & new_tas_avg$month<=5]<-"Off"
+new_tas_avg$snow[new_tas_avg$month>=11 & new_tas_avg$month<=12]<-"Off"
+#boxplot of albedo throughout seasons
 boxplot(
-  new_tas_avg$tas~new_tas_avg$season, 
+  new_tas_avg$tasman~new_tas_avg$season, 
   new_tas_avg,
   main = "Tasman Glacier Avg Albedo vs. Season (2000-2020)",
   xlab = "Season",
   ylab = "Average Albedo")
 
+###################################
+#
+# subset for snow on/off months (avg datasets only)
+# 
 
+fj_snow_on <- subset(new_fj_avg,snow=='On')
+fj_snow_off <- subset(new_fj_avg,snow=='Off')
+
+tas_snow_on <- subset(new_tas_avg,snow=='On')
+tas_snow_off <- subset(new_tas_avg,snow=='Off')
+
+fox_snow_on <- subset(new_fox_avg,snow=='On')
+fox_snow_off <- subset(new_fox_avg,snow=='Off')
